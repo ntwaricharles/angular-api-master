e1376ca66811c2614a6e9984da906b44b56ec811
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-14 22:27:05 +0200

    angular api master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: PostsListComponent },
+  { path: 'post/create', component: CreatePostComponent },
+  { path: 'post/edit/:id', component: EditPostComponent },
+  {
+    path: 'post/:id',
+    loadChildren: () =>
+      import('./components/post-detail/post-detail.module').then(
+        (m) => m.PostDetailModule
+      ),
+
+  },
+  { path: '**', redirectTo: '/posts' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    CreatePostComponent,
+    EditPostComponent,
+    PaginationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
