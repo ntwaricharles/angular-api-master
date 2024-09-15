13d6509b3f0b51fce0e2b4a54f302fedbb0e5596
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-15 19:42:00 +0200

    delete post model

--- src/app/components/delete-modal/delete-modal.component.spec.ts
+++ src/app/components/delete-modal/delete-modal.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DeleteModalComponent } from './delete-modal.component';
 
 describe('DeleteModalComponent', () => {
@@ -8,9 +7,8 @@ describe('DeleteModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DeleteModalComponent]
-    })
-    .compileComponents();
+      declarations: [DeleteModalComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteModalComponent);
     component = fixture.componentInstance;
@@ -19,5 +17,36 @@ describe('DeleteModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('openModal', () => {
+    it('should set isVisible to true', () => {
+      component.openModal();
+      expect(component.isVisible).toBe(true);
+    });
+  });
+
+  describe('closeModal', () => {
+    it('should set isVisible to false and emit onCancel', () => {
+      component.isVisible = true; // Set to true to simulate the modal being open
+      spyOn(component.onCancel, 'emit');
+
+      component.closeModal();
+
+      expect(component.isVisible).toBe(false);
+      expect(component.onCancel.emit).toHaveBeenCalled();
+    });
+  });
+
+  describe('confirmDelete', () => {
+    it('should set isVisible to false and emit onConfirm', () => {
+      component.isVisible = true; // Set to true to simulate the modal being open
+      spyOn(component.onConfirm, 'emit');
+
+      component.confirmDelete();
+
+      expect(component.isVisible).toBe(false);
+      expect(component.onConfirm.emit).toHaveBeenCalled();
+    });
   });
 });
